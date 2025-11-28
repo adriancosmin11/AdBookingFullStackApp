dc542af053932a110f19087a80e4f4138116c4fc
Adrian Cosmin <zaevil03@gmail.com>
2025-11-28 14:01:16 +0200

    fix for live deployment

--- frontend/src/api/axiosClient.ts
+++ frontend/src/api/axiosClient.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:8080/api/v1',
+
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080/api/v1',
     headers: {
         'Content-Type': 'application/json',
     },
 });
-
 export default axiosClient;
